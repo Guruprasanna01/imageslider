42848901f920558a48786dccbd7d8c1d410b9a81
Guru prasanna <guruprasanna151@gmail.com>
2025-02-27 20:01:45 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const back = document.getElementById("back");
+const next = document.getElementById("next");
+const move = document.querySelector(".gallery");
+
+back.addEventListener("click",function(){
+    move.style.scrollBehavior = "smooth"
+    move.scrollLeft-=930;
+})
+
+next.addEventListener("click",()=>{
+    move.style.scrollBehavior = "smooth"
+    move.scrollLeft+=930;
+})
