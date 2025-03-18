a9601cbc1470338b6997323eeb4c59fcaee7e2fd
Guru prasanna <guruprasanna151@gmail.com>
2025-03-18 12:46:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const back = document.getElementById("back");
 const next = document.getElementById("next");
 const move = document.querySelector(".gallery");
 
-back.addEventListener("click",function(){
+back.addEventListener("click",()=>{
     move.style.scrollBehavior = "smooth"
     move.scrollLeft-=930;
 })
